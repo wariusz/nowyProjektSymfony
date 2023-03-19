7919a2fc4a35257583ef1eec5bcf916c65d7a9bc
Mariusz Walendzewicz <mwalendzewicz@gmail.com>
2023-03-19 21:45:27 +0100

    Dodanie użytkownika do bazy i ustawienie przekierowania po zalogowaniu

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
+        $this->passwordHasher = $passwordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $user = new User();
+        $user->setUsername('user');
+        $user->setEmail('user@localhost.localhost');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword(
+            $user,
+            'hasło'
+        ));
+        $manager->persist($user);
         $manager->flush();
     }
 }
