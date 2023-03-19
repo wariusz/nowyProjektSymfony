76f8c79a9fbc3af74801d32e4922cb07757b82b0
Mariusz Walendzewicz <mwalendzewicz@gmail.com>
2023-03-19 22:40:35 +0100

    Spolczenie formularza rejestracji

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +19,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextareaType::class, [
+                'label' => 'Nazwa użytkownika'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adres email'
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Akceptuje warunki',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
@@ -27,8 +35,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+                'label' => 'Hasło',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
