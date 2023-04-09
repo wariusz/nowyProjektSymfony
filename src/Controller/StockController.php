234bc1138843c622ae5e22b464b6545feb5d0641
Mariusz Walendzewicz <mwalendzewicz@gmail.com>
2023-04-09 18:13:41 +0200

    Dodawanie nowego magazynu: kontroler, widok, formularz

---
+++ src/Controller/StockController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Stock;
+use App\Form\StockFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StockController extends AbstractController
+{
+    #[Route('/stock', name: 'app_stock')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $stock = new Stock();
+        $form = $this->createForm(StockFormType::class, $stock);
+        $form->handleRequest($request);
+        $stock->setCreatedDate(new \DateTimeImmutable("now"));
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($stock);
+            $entityManager->flush();
+
+            return $this->render('stock/index.html.twig', ['newStock' => true]);
+        }
+
+        return $this->render('stock/index.html.twig', [
+            'stockForm' => $form->createView(),
+        ]);
+    }
+}
