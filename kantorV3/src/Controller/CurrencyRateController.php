1f6917759e2a1ea63317a15932628a3c96a45ce2
Mariusz Walendzewicz <mwalendzewicz@gmail.com>
2023-05-07 21:33:35 +0200

    kantorV3 - serwis i endpoint dodający dany kurs

--- kantorV3/src/Controller/CurrencyRateController.php
+++ kantorV3/src/Controller/CurrencyRateController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CurrencyRateController extends AbstractController
@@ -15,5 +17,25 @@ class CurrencyRateController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/CurrencyRateController.php',
         ]);
+    }
+
+    /**
+     * @Route("/api/currency-rates", name="add_currency_rate", methods={"POST"})
+     */
+    public function addCurrencyRate(Request $request, CurrencyRateService $currencyRateService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $currency = $data['currency'] ?? null;
+        $rate = $data['rate'] ?? null;
+        $date = $data['date'] ?? null;
+
+        if (!$currency || !$rate || !$date) {
+            return new JsonResponse(['error' => 'Invalid request data.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $currencyRate = $currencyRateService->addCurrencyRate($currency, $rate, $date);
+
+        return new JsonResponse(['id' => $currencyRate->getId()], Response::HTTP_CREATED);
     }
 }
---
+++ kantorV3/src/Controller/CurrencyRateService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CurrencyRate;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CurrencyRateService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function addCurrencyRate(string $currency, float $rate, string $date): CurrencyRate
+    {
+        $currencyRate = new CurrencyRate();
+        $currencyRate->setCurrency($currency);
+        $currencyRate->setRate($rate);
+        $currencyRate->setDate($date);
+
+        $this->entityManager->persist($currencyRate);
+        $this->entityManager->flush();
+
+        return $currencyRate;
+    }
+}
